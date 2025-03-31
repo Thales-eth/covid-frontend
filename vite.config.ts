4b383ae7d4d9afeb99362c6b93e7d29b26591ca9
Thales-eth <dan.console.log@gmail.com>
2025-03-31 23:31:13 +0200

    add import alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@/components": "/src/components",
+      "@/consts": "/src/consts",
+      "@/config": "/src/config",
+      "@/services": "/src/services",
+      "@/types": "/src/types",
+      "@/utils": "/src/utils",
+    },
+  },
 })
