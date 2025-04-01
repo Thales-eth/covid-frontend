f89202f59a6ba8de1f1e17d676cfe4664e70c3fe
Thales-eth <dan.console.log@gmail.com>
2025-04-01 23:36:39 +0200

    add loading overlay

---
+++ src/components/LoadingOverlay/LoadingOverlay.component.tsx
@@ -0,0 +1,16 @@
+import Spinner from "../Spinner/Spinner.component"
+
+interface ILoadingOverlayProps {
+  title: string
+}
+
+const LoadingOverlay = ({ title }: ILoadingOverlayProps) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-white/25 backdrop-blur-sm">
+      <p className="body-medium">{title}</p>
+      <Spinner size={10} />
+    </div>
+  )
+}
+
+export default LoadingOverlay
