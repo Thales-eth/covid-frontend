6a79ad6e1ad1e9f5e18d96aed08bbef6f6b01b14
Thales-eth <dan.console.log@gmail.com>
2025-04-01 23:33:09 +0200

    add react router dom initial routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,22 @@
 import { createBrowserRouter } from "react-router-dom"
 import CovidPage from "@/pages/CovidPage"
 import HomePage from "@/pages/Home"
+import SideBar from "@/components/SideBar/SideBar.component"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/covid",
-    element: <CovidPage />,
+    element: <SideBar />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/covid",
+        element: <CovidPage />,
+      },
+    ],
   },
 ])
 
