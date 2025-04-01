76cb1aa32197e5f0b36c7dff21b281b3b3381027
Thales-eth <dan.console.log@gmail.com>
2025-04-01 23:31:43 +0200

    init tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@/components": "/src/components",
@@ -14,6 +15,7 @@ export default defineConfig({
       "@/utils": "/src/utils",
       "@/routes": "/src/routes",
       "@/pages": "/src/pages",
+      "@/assets": "/src/assets",
     },
   },
 })
