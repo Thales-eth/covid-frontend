0937abf9cead1fa58c6e2abb4891e29971ef3520
Thales-eth <dan.console.log@gmail.com>
2025-04-03 00:54:07 +0200

    add default api url to axios config

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_URL } from "../consts/env.consts";
 
 export const apiClient = axios.create({
-  baseURL: API_URL,
+  baseURL: API_URL || "http://localhost:3200/api",
   headers: {
     "Content-Type": "application/json",
   },
